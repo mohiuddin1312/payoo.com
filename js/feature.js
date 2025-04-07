391c59bcd8d6177efad6cc1d34242b05e4baee01
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-04-07 11:57:50 +0600

    finish feature file

--- js/feature.js
+++ js/feature.js
@@ -1,3 +1,11 @@
 document.getElementById("btn-cash-out").addEventListener("click", function () {
-  document.getElementById("cash-out-file").classList.remove("hiddin");
+  document.getElementById("cash-out-file").classList.remove("hidden");
+  document.getElementById("add-money-file").classList.add("hidden");
+});
+
+// another Addmoney button
+document.getElementById("btn-add-money").addEventListener("click", function () {
+  document.getElementById("add-money-file").classList.remove("hidden");
+  document.getElementById("cash-out-file").classList.add("hidden");
+  console.log("add money");
 });
